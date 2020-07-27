627281dcdeb0717aa657aa30b528f29102797059
peawarrior <jacksonchen171@gmail.com>
2020-07-27 12:44:40 -0400

    added index and new to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+
+    before_action :set_post
+
+    def index
+        #change this to current_user.posts
+        @posts = Post.all
+    end
+
+    def new
+        #authorize user before creating
+    end
+
+
+    private
+
+    def set_post
+        @post = Post.new if params[:id]
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :content, :tags)
+    end
 end
