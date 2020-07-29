5e7d11afa185947401cc0ceebe4edaf2ae909d4c
peawarrior <jacksonchen171@gmail.com>
2020-07-29 16:17:39 -0400

    added searchbar(by) feature, posts format feature

--- db/migrate/20200729014133_add_meditation_dates_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddMeditationDatesToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :meditation_dates, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -86,9 +86,9 @@ ActiveRecord::Schema.define(version: 2020_07_29_133409) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.text "meditation_dates"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "meditation_dates"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
